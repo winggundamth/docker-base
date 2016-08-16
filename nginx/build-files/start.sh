f71da023343168b7418819584f8cfb039c5e46e9
Jirayut 'Dear' Nimsaeng <jirayut.n@kaidee.com>
2016-08-16 19:32:25 +0700

    Improve Dockerfile when remove build-essential and change from COPY to ADD

--- nginx/build-files/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf /var/run/rsyslogd.pid
-service rsyslog start
-# Work around fix cron not working on overlayfs as describe here
-# https://github.com/docker/docker/issues/16813
-touch /etc/crontab
-cron
-nginx
